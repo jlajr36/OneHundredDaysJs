5a4ea3ddeb55ff8698a169397d2f8f396d6e58d6
unknown <jlajr36@gmail.com>
2020-02-11 19:39:44 -0600

    File Changes

---
+++ BeginningNodeJS/varTest.js
@@ -0,0 +1,10 @@
+var foo = 1;
+var bar = 2;
+
+console.log(foo);
+console.log(bar);
+console.log(foo - bar);
+console.log(foo+bar);
+console.log(foo*bar);
+console.log(foo/bar);
+console.log(foo % 2);
