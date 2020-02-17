3b42002aa36e15cb83f50a56e21b901e2f8f19c1
unknown <jlajr36@gmail.com>
2020-02-17 04:32:39 -0600

    Basic JS

---
+++ BasicJavaScript/BasicJS_1.js
@@ -0,0 +1,88 @@
+//Basic Javascript Line Comment
+
+/* A Multi Line
+Comment*/
+
+//Add comments to code in order make notes for yourself and others
+
+/*Types of variables
+    undefined, null, boolean, string, symbol, number, object
+*/
+
+//variables point to data not the data itself
+
+var myName = "John";
+console.log(myName);
+
+var a;
+var b = 2;
+a = 7;
+b = a;
+console.log(b);
+
+var a = 9;
+console.log(a);
+
+var c = 5;
+var d = 10;
+var e = "I am a";
+
+c = c + 1;
+d = d + 5;
+e = e + " String";
+
+console.log(c);
+console.log(d);
+console.log(e);
+
+//variables and functions are case sensitive
+//use camelCase to name variables
+
+//Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+//Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Use
+console.log(studlyCapVar);
+console.log(properCamelCase);
+console.log(titleCaseOver);
+
+//Add
+var sum = 10 + 10;
+console.log(sum);
+
+//Sub
+var difference = 45 - 33;
+console.log(difference);
+
+//Multiply
+var product = 8 * 10;
+console.log(product);
+
+//Divide
+var quotient = 66/33;
+console.log(quotient);
+
+//Increment
+var myVar = 87;
+myVar++;
+console.log(myVar);
+
+//Decrement
+myVar--;
+console.log(myVar);
+
+//Decimal
+var myDecimal = 5.7;
+myDecimal = 5.7 + 2.2;
+console.log(myDecimal);
+
+//Mutiply Two Decimals
+var productTwo = 2.0 * 2.5;
+console.log(productTwo);
